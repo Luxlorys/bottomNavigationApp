bce187a9b6bca95be35111f9afa8ecc6f81018b4
luxlorys <bossbyrcev221@gmail.com>
2023-04-21 14:24:28 +0300

    removed title bar

--- app/src/main/java/com/example/bottomnavigationapp/MainActivity.kt
+++ app/src/main/java/com/example/bottomnavigationapp/MainActivity.kt
@@ -12,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        this.supportActionBar?.hide()
 
         val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
 
